2a34529fa56b81850c8c0bf029c95f3fc95361c2
lengmo714 <842673413@qq.com>
2018-08-19 01:41:00 +0800

    删了一个文件

--- 手套代码 - 虚拟手/手套代码 - 虚拟手/HARDWARE/ADC/adc.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ADC_H
-#define __ADC_H	
-#include "sys.h"
-
-//ADC≈‰÷√
-void Adc_Init(void);
- 
-#endif 
--- 手套代码 - 虚拟手/手套代码 - 虚拟手/HARDWARE/DMA/dma.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DMA_H
-#define	__DMA_H	   
-#include "sys.h"
-
-//≈‰÷√DMA1_CHx
-void MyDMA_Init(DMA_Channel_TypeDef *DMA_CHx,u32 cpar,u32 cmar,u16 cndtr);
-		   
-#endif
